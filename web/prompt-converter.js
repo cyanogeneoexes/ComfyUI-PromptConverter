36e97488e339ae4e14d3a86a876ae300a78ab8b4
senyuhsi <senyuhsi@gmail.com>
2025-01-19 14:57:51 +0900

    方針変更

--- web/prompt-converter.js
+++ web/prompt-converter.js
@@ -1,39 +1,29 @@
 import { app } from "../../scripts/app.js";
 
 
+/* ノード選択サンプル
+const nodes = app.graph._nodes;
+const targetNode = nodes.find(node => node.type === "LoadImage");
+*/
+
+/* タグ検索サンプル
+const url = "https://danbooru.donmai.us/related_tag?commit=Search&search%5Bcategory%5D=General&search%5Border%5D=Cosine&search%5Bquery%5D=kantai_collection";
+const response = await fetch(url);
+const data = await response.json();
+console.log(data);
+*/
+
+
 app.registerExtension({
     name: "Prompt Converter",
     async setup() {
         const searchUploadSetting = app.ui.settings.addSetting({
-            id: "searchUploadNode",
-            name: "Search Upload Node",
+            id: "searchRelatedTag",
+            name: "Search Related Tag",
             category: ["Prompt Converter", "Settings"],
             type: "boolean",
             defaultValue: false,
             tooltip: "サンプル設定の説明文"
-        });
-
-        // Ctrlが押されているかどうかを追跡
-        let isCtrlPressed = false;
-
-        document.addEventListener('keydown', async (e) => {
-            // Ctrlキーが押された時の処理
-            if (e.key === 'Control' && !isCtrlPressed && searchUploadSetting.value) {
-                isCtrlPressed = true;
-                const nodes = app.graph._nodes;
-                const targetNodes = nodes.filter(node => node.type === "LoadImage");
-                
-                if (targetNodes.length > 0) {
-                    app.canvas.selectNodes(targetNodes);
-                }
-            }
-        });
-
-        // Ctrlキーが離された時のフラグリセット
-        document.addEventListener('keyup', (e) => {
-            if (e.key === 'Control') {
-                isCtrlPressed = false;
-            }
         });
     }
 });
